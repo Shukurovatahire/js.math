742f2c9845ddcbdd31f662e632fb2caf8fa652b1
Shukurova Tahire <sukurovatahire08@gmail.com>
2024-03-07 01:22:14 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// ? Task 1 Start
+
+// const RectangleArea = () => {
+//   let length = +prompt("Enter length of rectangle: ");
+//   let width = +prompt("Enter width of rectangle: ");
+
+//   const maxLength = Math.max((length, width));
+//   const minWidth = Math.min(length, width);
+//   const area = maxLength * minWidth;
+//   console.log('Width',minWidth);
+//   console.log("Length",maxLength);
+//   return alert(`Area of the rectangle: ${area}`);
+// };
+
+console.log(RectangleArea());
+
+// ? Task 1 End.
+
+
+//? Task 2 Start:
+
+// const box = document.getElementById("box");
+
+// const randomColor = () => {
+//   let r = Math.floor(Math.random() * 256);
+//   let g = Math.floor(Math.random() * 256);
+//   let b = Math.floor(Math.random() * 256);
+
+//   let colorCode = `rgb(${r}, ${g}, ${b})`;
+//   box.style.backgroundColor = colorCode;
+// };
+
+// box.addEventListener("click", randomColor);
+
+// randomColor();
+
+//? Task 2 end.
